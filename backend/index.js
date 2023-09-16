d0540b1066ed4420e44f02a60b7093db40c723d1
piyush-c38 <chandrakarpiyush36@gmail.com>
2023-09-17 01:10:58 +0530

    FEAT: adding new file structure

--- backend/index.js
+++ backend/index.js
@@ -1,34 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
-
-// set up express
+const passport = require("passport");
 
 const app = express();
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(passport.initialize());
+
+// Import API routes
+const authRoutes = require("./routes/auth");
+const podcastRoutes = require("./routes/podcasts.js");
+
+// Connect to MongoDB Atlas
+mongoose.connect("mongodb+srv://chandrakarpiyush36:1234@cluster0.cuvsccq.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB Atlas"))
+.catch((err) => console.log("Error connecting to MongoDB Atlas:", err));
+
+// Use API routes
+app.use("/api/auth", authRoutes);
+app.use("/api/podcasts", podcastRoutes);
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
-
-// set up mongoose
-
-mongoose.connect(
-  process.env.MONGODB_CONNECTION_STRING,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  },
-  (err) => {
-    if (err) throw err;
-    console.log("MongoDB connection established");
-  }
-);
-
-// set up routes
-
-app.use("/users", require("./routes/users"));
-app.use("/todos", require("./routes/todo"));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,53 +1,38 @@
-import React, {useState, useEffect } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import axios from 'axios';
-import Header from './components/layout/Header';
-import Home from './components/pages/Home';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import UserContext from './context/userContext';
-import './App.css';
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-function App() {
-  const [ userData, setUserData] = useState({
-    token: undefined,
-    user: undefined
-  });
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
+import PodcastDetail from "./components/PodcastDetail";
+import AdminPanel from "./components/AdminPanel";
+import AddPodcast from "./components/AddPodcast";
 
-  useEffect(() => {
-    const checkLoggedIn = async () => {
-      let token = localStorage.getItem("auth-token");
-      if(token === null){
-        localStorage.setItem("auth-token", "");
-        token = "";
-      }
-      const tokenResponse = await axios.post('http://localhost:5000/users/tokenIsValid', null, {headers: {"x-auth-token": token}});
-      if (tokenResponse.data) {
-        const userRes = await axios.get("http://localhost:5000/users/", {
-          headers: { "x-auth-token": token },
-        });
-        setUserData({
-          token,
-          user: userRes.data,
-        });
-      }
-    }
-
-    checkLoggedIn();
-  }, []);
-
+const App = () => {
   return (
     <BrowserRouter>
-      <UserContext.Provider value={{ userData, setUserData }}>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-        </Switch>
-        </UserContext.Provider>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element ={<Login />} />
+      </Routes>
+      <Routes>
+        <Route path="/signup" element={<Signup /> } />
+      </Routes>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+      <Routes>
+        <Route path="/podcasts/:id" element={<PodcastDetail />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/admin" element={<AdminPanel />} />
+      </Routes>
+      <Routes>
+        <Route path="/admin/podcasts/add" component={AddPodcast} />
+      </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
