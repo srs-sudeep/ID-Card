0eb62d7401194a7c1871ad43f766ac3720112151
Nishchay Rajput <nishchayr@iitbhilai.ac.in>
2023-09-19 22:29:49 +0530

    Fix : parsing data into history

--- web-client/src/_mock/user.js
+++ web-client/src/_mock/user.js
@@ -1,8 +1,10 @@
 import { faker } from '@faker-js/faker';
 import { sample } from 'lodash';
-
+import account from './account';
 // ----------------------------------------------------------------------
-
+// console.log(account);
+const users1 =;
+console.log(users1);
 const users = [...Array(24)].map((_, index) => ({
   id: faker.datatype.uuid(),
   avatarUrl: `/assets/images/avatars/avatar_${index + 1}.jpg`,
